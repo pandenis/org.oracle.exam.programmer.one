598865ed6db1bfcbe2001a43befa1841f5d51a62
pandenis <pan.denis@gmail.com>
2021-05-01 21:23:46 +0300

    primitives data types

---
+++ src/lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,59 @@
+package lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 100000000000L;
+
+        float f1 = 3.14F;
+        float f2 = 2.4f;
+        float f3 = 20;
+
+        double d1 = 5.5;
+        double d2 = 85.58;
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = ' ';
+
+        char c5 = 500;
+
+        char c6 = '\u05AB';
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(c5);
+        System.out.println(c6);
+
+        int a1 = 60;
+        System.out.println(a1);
+
+        int a2 = 0B111100;
+        System.out.println(a2);
+
+        int a3 = 0b111100;
+        System.out.println(a3);
+
+        int a4 = 0_74;
+        System.out.println(a4);
+
+        int a5 = 0x3C;
+        System.out.println(a5);
+
+
+        int a7 = 1_000_000;
+
+    }
+}
