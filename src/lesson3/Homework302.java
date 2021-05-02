a96ef73e72d924081afd6e5dec63bb2a38956327
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-02 11:51:24 +0300

    arithmetic ex 2 + Homework301.java

--- src/lesson3/Homework302.java
+++ src/lesson3/Homework302.java
@@ -6,5 +6,14 @@ public class Homework302 {
         int r = a-- - --a + ++a + a++ + a;
              // 5   -   3 +   4 + 4   + 5
         System.out.println(r);
+
+        int b = 8;
+        System.out.println(++b); //9
+        System.out.println(b++); //9
+        System.out.println(++b); //10 + 1 = 11
+        System.out.println(--b); //10
+        int q = ++b - b++ + ++b - --b;
+
+        System.out.println(q);
     }
 }
