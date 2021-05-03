8f5fb21a370747ed062e7dc6be354ef43baea895
pandenis <pan.denis@gmail.com>
2021-05-03 23:04:47 +0300

    lesson5 BankAccountNew with refill method

---
+++ src/lesson5/homework501/BankAccountNew.java
@@ -0,0 +1,18 @@
+package lesson5.homework501;
+
+public class BankAccountNew {
+
+    public BankAccountNew(int id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    int id;
+    String name;
+    double balance;
+
+    int refill(int sum) {
+        return id += sum;
+    }
+}
