c696195d7c738d851e54175b26da58e72291ee5d
pandenis <pan.denis@gmail.com>
2021-05-03 19:32:29 +0300

    lesson4 Class Object
    Student.java Create Object

---
+++ src/lesson4/homework401/Student.java
@@ -0,0 +1,11 @@
+package lesson4.homework401;
+
+public class Student {
+    int gradeSheet;
+    String firstName;
+    String secondName;
+    int yearOfStudy;
+    double avgMath;
+    double avgEconomic;
+    double avgForeignLanguage;
+}
