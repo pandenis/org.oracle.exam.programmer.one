10bc7c6b29f79798f36268b77cb803cdcf7b6efa
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-09 15:24:02 +0300

    lesson08 homework0801 creation and usage in class and it's methods

---
+++ src/lesson08/homework0801/Main.java
@@ -0,0 +1,13 @@
+package lesson08.homework0801;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Test0801 t1 = new Test0801();
+        System.out.println(t1.sum(4, 6, 2));
+        System.out.println(t1.sum(5, 5, 4));
+        t1.devise(8, 3);
+        t1.devise(9, 2);
+    }
+
+}
---
+++ src/lesson08/homework0801/Test0801.java
@@ -0,0 +1,13 @@
+package lesson08.homework0801;
+
+public class Test0801 {
+    public String sum(int a, int b, int c) {
+        return "The sum is: " + (a + b + c);
+    }
+
+    public void devise(int a, int b) {
+        int d = a / b;
+        int m = a % b;
+        System.out.println("Quotient of division is: " + d + " and remainder is: " + m);
+    }
+}
