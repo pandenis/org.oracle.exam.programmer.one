cf272c4aba9acfd199e349094a920c2033d67bf1
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-09 16:27:42 +0300

    lesson08 homework0801 creation and usage in class and it's methods with access modifiers

---
+++ src/lesson08/homework0802/Circle.java
@@ -0,0 +1,21 @@
+package lesson08.homework0802;
+
+public class Circle {
+    static final double pi = 3.14;
+
+
+    double squareCalc (int r) {
+        return pi * r + r;
+    }
+
+    static double circumferenceCalc (int r) {
+        return 2 * (pi * r + r) * r;
+    }
+
+    void printCircle(int r) {
+
+        System.out.println("The radius of circle is: " + r);
+        System.out.println("The square of circle when radius is" + r + "equal to" + this.squareCalc(r));
+        System.out.println("The circumference of circle when radius is" + r + "equal to" + circumferenceCalc(r));
+    }
+}
---
+++ src/lesson08/homework0802/Main.java
@@ -0,0 +1,10 @@
+package lesson08.homework0802;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.squareCalc(5);
+        Circle.circumferenceCalc(4);
+        circle.printCircle(5);
+    }
+}
