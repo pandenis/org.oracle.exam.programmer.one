ded01d0ed653f7f41e8867e24ae3898a85489c2f
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-09 16:37:22 +0300

    lesson08 homework0801 fix methods to static

--- src/lesson08/homework0801/Test0801.java
+++ src/lesson08/homework0801/Test0801.java
@@ -1,13 +1,13 @@
 package lesson08.homework0801;
 
 public class Test0801 {
-    public String multiply(int a, int b, int c) {
+    public static String multiply(int a, int b, int c) {
         return "The multiply is: " + (a * b * c);
     }
 
-    public void devise(int a, int b) {
+    public static void devise(int a, int b) {
         int d = a / b;
         int m = a % b;
-        System.out.println("Quotient of division is: " + d + " and remainder is: " + m);
+        System.out.println("Quotient of division " + a + " and " + b + " is: " + d + " and remainder is: " + m);
     }
 }
