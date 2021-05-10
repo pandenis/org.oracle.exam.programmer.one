a5866295c1d881fe8ffb70f57fb2804490e2ee83
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-10 16:10:58 +0300

    lesson09 homework0902 variables priority and statics

---
+++ src/lesson09/homework0902/Test1.java
@@ -0,0 +1,16 @@
+package lesson09.homework0902;
+
+public class Test1 {
+    int a = 1;
+//    static int a = 2;
+    void abc(int a) {
+        System.out.println(a);
+        System.out.println(this.a);
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        t.abc(3);
+    }
+}
+
