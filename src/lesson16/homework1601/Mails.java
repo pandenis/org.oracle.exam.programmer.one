4af6b0309702c21e0ab6e6857b8e69b0230ef0a2
pandenis <pan.denis@gmail.com>
2021-05-18 17:23:57 +0300

    lesson16 String HOMEWORK...... not completed

--- src/lesson16/homework1601/Mails.java
+++ src/lesson16/homework1601/Mails.java
@@ -3,24 +3,24 @@ package lesson16.homework1601;
 public class Mails {
 
     public void email(String list) {
-       for (int i = 0; i < list.length(); i++) {
-            String domain = list.substring(list.indexOf("@") + 1,list.indexOf(";"));
-            System.out.println(domain);
 
-           INNER: if (list.charAt(i) == ';') {
-                String temp = list.substring(list.indexOf(i));
-                System.out.println(temp);
-                break INNER;
+        list = list.trim();
+
+        for (int i = 0; i < list.length(); i++) {
+            char a = list.charAt(i);
+
+            if (a == '@') {
+                int s = list.indexOf(';');
+                String domain = list.substring(0, s).substring(++i);
+                list = list.substring(++s);
+                System.out.println(domain);
+                i = 0;
             }
         }
-
-
     }
-
-
 
     public static void main(String[] args) {
         Mails mails = new Mails();
-        mails.email("ya@yahoo.com; on@mail.com; ona@gmail.com");
+        mails.email("ya@yahoo.com; on@mail.com; ona@gmail.com;");
     }
 }
