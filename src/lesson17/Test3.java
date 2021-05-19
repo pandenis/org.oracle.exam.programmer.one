f6a2a826fa4d7ba4174a2c9b31cbb5e8d62b670f
pandenis <pan.denis@gmail.com>
2021-05-19 22:48:52 +0300

    lesson17 StringBuilder

---
+++ src/lesson17/Test3.java
@@ -0,0 +1,11 @@
+package lesson17;
+
+public class Test3 {
+    public static void main(String[] args) {
+        StringBuffer sb1 = new StringBuffer("Hello");
+        StringBuffer sb2 = new StringBuffer("Good bye");
+
+        String s1 = new String(sb1);
+        String s2 = new String(sb2);
+    }
+}
