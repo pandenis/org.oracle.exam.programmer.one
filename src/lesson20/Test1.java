c88def05683aae1306a478815aad7c17c3e637cd
pandenis <pan.denis@gmail.com>
2021-05-23 09:44:15 +0300

    lesson20 ArrayList

---
+++ src/lesson20/Test1.java
@@ -0,0 +1,15 @@
+package lesson20;
+
+import java.util.ArrayList;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("privet");
+        list.add(new StringBuilder("ddd"));
+
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("bye");
+
+    }
+}
