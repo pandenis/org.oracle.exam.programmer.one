aa1fe93faf24112612ec2e3cee83ef2ac6cb1e25
denisp <denis.pergamenstschik@cu-bx.com>
2021-05-23 11:58:26 +0300

    String LastIndexOF

---
+++ src/temp/LastIndexOF.java
@@ -0,0 +1,10 @@
+package temp;
+
+public class LastIndexOF {
+    public static void main(String[] args) {
+        String string = "DenisDenisDenis";
+        int d = string.lastIndexOf('D');
+        System.out.println(string.length());
+        System.out.println(d);
+    }
+}
